3daa8ea3d157ed14b19d18967ccad898da170568
Prasith Prabhu <prasithtoughboy@gmail.com>
2020-05-08 16:02:22 +0530

    Changed the path

--- gke/gcp.tf
+++ gke/gcp.tf
@@ -2,7 +2,7 @@
 # Google Cloud Platform
 #####################################################################
 provider "google" {
-  credentials = "${file("/home/vvdn/Downloads/gcloud-sa.json")}"
+  credentials = "${file("/home/path/Downloads/gcloud-sa.json")}"
   project = "${var.project}"
   region  = "${var.region}"
 }
